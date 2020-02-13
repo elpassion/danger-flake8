34070a0d067c75e0e85ea7229d81ee68df82cdcc
Maciej Gomółka <mgomolka92@gmail.com>
2020-02-13 11:36:01 +0100

    Rename test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,7 +3,7 @@ from danger_python.danger import Danger, Violation
 from danger_flake8.plugin import DangerFlake8
 
 
-def test_plugin_lint(danger: Danger):
+def test_lint(danger: Danger):
     plugin = DangerFlake8()
     plugin.lint()
 
