ef0a9d848268b34ea1558ba32acda3ef218482d2
Maciej Gomółka <mgomolka92@gmail.com>
2020-02-13 16:28:15 +0100

    Add array of warnings instead of markdown

--- danger_flake8/plugin.py
+++ danger_flake8/plugin.py
@@ -2,9 +2,13 @@ import subprocess
 
 from danger_python.plugins import DangerPlugin
 
+from danger_flake8.violation import violations
+
 
 class DangerFlake8(DangerPlugin):
     def lint(self):
         result = subprocess.run(["flake8"], capture_output=True, text=True)
-        output = result.stdout
-        self.markdown(output)
+        violations_list = violations(result.stdout)
+
+        for violation in violations_list:
+            self.warn(violation.message, violation.file_name, violation.line)
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -13,12 +13,20 @@ def test_lint_run_flake8_subprocess(danger: Danger):
             plugin = DangerFlake8()
             plugin.lint()
 
-    assert danger.results.markdowns == [
+    assert danger.results.warnings == [
         Violation(
-            message=(
-                "./danger_flake8/plugin.py:2:1: F811 redefinition of unused 'DangerPlugin' from line 1\n"
-                "./danger_flake8/plugin.py:6:1: E303 too many blank lines (3)\n"
-                "./danger_flake8/plugin.py:11:1: W391 blank line at end of file"
-            )
+            message="F811 redefinition of unused 'DangerPlugin' from line 1",
+            file_name="danger_flake8/plugin.py",
+            line=2,
+        ),
+        Violation(
+            message="E303 too many blank lines (3)",
+            file_name="danger_flake8/plugin.py",
+            line=6,
+        ),
+        Violation(
+            message="W391 blank line at end of file",
+            file_name="danger_flake8/plugin.py",
+            line=11,
         )
     ]
