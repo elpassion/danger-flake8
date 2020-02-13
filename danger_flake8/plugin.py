d99372ecb29156c5461552cf128aacb14df7e650
Maciej Gomółka <mgomolka92@gmail.com>
2020-02-13 12:49:24 +0100

    Run flake8 subprocess

--- danger_flake8/plugin.py
+++ danger_flake8/plugin.py
@@ -1,6 +1,9 @@
+import subprocess
+
 from danger_python.plugins import DangerPlugin
 
 
 class DangerFlake8(DangerPlugin):
     def lint(self):
-        self.fail("fail")
+        result = subprocess.run(["flake8"], capture_output=True, text=True)
+        self.danger.results.markdowns = [result.stdout.decode("utf-8")]
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,11 +1,25 @@
-from danger_python.danger import Danger, Violation
+import subprocess
+from typing import List, Dict, Any
+from unittest.mock import patch
 
-from danger_flake8.plugin import DangerFlake8
+from danger_python.danger import Danger
+
+from danger_flake8 import DangerFlake8
 
 
-def test_lint(danger: Danger):
-    plugin = DangerFlake8()
-    plugin.lint()
+def test_lint_run_flake8_subprocess(danger: Danger):
+    def fake_run(commands: List[str], **kwargs: Dict[str, Any]) -> subprocess.CompletedProcess:
+        if (
+                commands == ["flake8"]
+                and kwargs["capture_output"] == True
+                and kwargs["text"] == True
+        ):
+            return subprocess.CompletedProcess(["flake8"], 0, stdout="output".encode("utf-8"))
+        raise ValueError(f"Could not stub commands: {commands}")
 
-    assert danger.results.markdowns == []
-    assert danger.results.fails == [Violation(message="fail")]
+    with patch("subprocess.run") as mock_subprocess:
+        mock_subprocess.side_effect = fake_run
+        plugin = DangerFlake8()
+        plugin.lint()
+
+    assert danger.results.markdowns == ["output"]
