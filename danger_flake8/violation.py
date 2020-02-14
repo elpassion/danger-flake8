25015b402c5c4e94a833817c3d67b2d14ed417f4
Maciej Gomółka <mgomolka92@gmail.com>
2020-02-14 10:34:32 +0100

    test

--- danger_flake8/violation.py
+++ danger_flake8/violation.py
@@ -1,19 +1,23 @@
 from typing import List, Optional
 
 from danger_python.danger import Violation
+import subprocess
 
 
 def violations(text_violations: str) -> List[Violation]:
-    if not text_violations.split():
+    if not text_violations:
         return []
     results = list(map(violation, text_violations.split("\n")))
     return list(filter(None, results))
 
 
 def violation(text_violation: str) -> Optional[Violation]:
-    if not text_violation.split():
+    if not text_violation:
         return
     message = text_violation.split(":")[3][1:]
     file_name = text_violation.split(":")[0][2:]
     line = text_violation.split(":")[1]
     return Violation(message=message, file_name=file_name, line=line)
+
+
+
