4c1f22ec9577f4a9aaf6ea081af0313e9e3644f1
Maciej Gomółka <mgomolka92@gmail.com>
2020-02-14 12:21:42 +0100

    Refactor violation.py

--- danger_flake8/violation.py
+++ danger_flake8/violation.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Iterator
 
 from danger_python.danger import Violation
 
@@ -6,13 +6,13 @@ from danger_python.danger import Violation
 def violations(text_violations: str, touched_files: Set[str]) -> List[Violation]:
     if not text_violations:
         return []
-    violations_list = filter(None, map(violation, text_violations.split("\n")))
+    violations_list: Iterator[Violation] = filter(None, map(violation, text_violations.split("\n")))
     return list(filter(lambda x: x.file_name in touched_files, violations_list))
 
 
 def violation(text_violation: str) -> Optional[Violation]:
     if not text_violation:
-        return
+        return None
     message = text_violation.split(":")[3][1:]
     file_name = text_violation.split(":")[0][2:]
     line = text_violation.split(":")[1]
